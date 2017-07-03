c58b22839234bf20e3c0ae7938de8ec137a78b32
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2017-07-03 14:05:13 +0200

    Fix styling2

--- webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
+++ webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
@@ -920,9 +920,10 @@ void ModuleRtpRtcpImpl::SetVideoBitrateAllocation(
 
 bool ModuleRtpRtcpImpl::SetMediaCryptoKey(
     const rtc::Optional<MediaCryptoKey>& media_crypto_key) {
-  if (rtp_sender_)
+  if (rtp_sender_) {
     // Set end to end media encryption key
     return rtp_sender_->SetMediaCryptoKey(media_crypto_key);
+  }
   return true;
 }
 
